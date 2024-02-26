c80637328b29e674c4a302390fdfee648bd22898
mateori0s <mateorios@live.com>
2024-02-26 12:14:38 -0300

    app.js

--- src/public/js/customActivity.js
+++ src/public/js/customActivity.js
@@ -12,11 +12,33 @@ define(['postmonger'], (Postmonger) => {
     });
     connection.on('initActivity', (data) => {
         if (data) payload = data;
+
+        const inArguments = Boolean(
+            data.arguments &&
+            data.arguments.execute &&
+            data.arguments.execute.inArguments &&
+            data.arguments.execute.inArguments.length > 0
+        ) ? data.arguments.execute.inArguments : [];
+
+        const dataExtensionArg = inArguments.find(arg => arg.dataExtension);
+        if (dataExtensionArg) document.getElementById('dataExtension').value = dataExtensionArg.dataExtension;
+        
+        const channelArg = inArguments.find(arg => arg.channel);
+        if (channelArg) document.getElementById('channel').value = channelArg.channel;
+   
     });
 
     connection.on('clickedNext', () => {
+        const dataExtension = document.getElementById('dataExtension').value;
+        const cellularNumber = `{{Contact.Attribute."${dataExtension}".cellular_number}}`;
+        const channel = document.getElementById('channel').value;
+
+
         payload['arguments'].execute.inArguments = [
-            { phone: `{{Contact.Attribute.PACKS_ADDITIONAL_DATA.CELLULAR_NUMBER}}` },
+            { dataExtension: dataExtension ? dataExtension : null },
+            { cellularNumber: cellularNumber ? cellularNumber : null },
+            { channel: channel ? channel : null },
+
         ];
         payload['metaData'].isConfigured = true;
         connection.trigger('updateActivity', payload);
