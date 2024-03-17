fb145ae8b36d75f4178081397629f9587edcc772
mateori0s <mateorios@live.com>
2024-03-17 16:15:24 -0300

    funcion modificada

--- src/server/routes/activity.ts
+++ src/server/routes/activity.ts
@@ -1,8 +1,6 @@
-'use strict';
 import https from 'https';
 import axios from 'axios';
-import { Request } from 'express';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 const logExecuteData: {
@@ -27,7 +25,7 @@ const logExecuteData: {
 
 const saveData = (req: any) => {
   // Put data from the request in an array accessible to the main app.
-  exports.logExecuteData.push({
+  logExecuteData.push({
     body: req.body,
     headers: req.headers,
     trailers: req.trailers,
@@ -52,13 +50,16 @@ interface InputParamenter {
   cellularNumber?: string;
   channel?: string;
 }
+
 interface DecodedBody {
   inArguments?: InputParamenter[];
 }
+
 interface DurationTimestampsPair {
   start: number | null;
   end: number | null;
 }
+
 interface RequestBody {
   cellularNumber: number;
   channel: string;
@@ -68,23 +69,18 @@ interface ResponseBody {
   responseCode: number;
   responseMessage: string;
   handle: number;
-//   {
-//     code: number,
-//     description: string
-// }
   pack: {
-    packId: string,
-    description: string
-}[];
+    packId: string;
+    description: string;
+  }[];
 }
-
 
 const execute = async function (req: Request, res: Response) {
   const { body } = req;
   const { env: { JWT_SECRET } } = process;
 
   if (!body) {
-    console.error(new Error('invalid jwtdata'));
+    console.error(new Error('Invalid jwtdata'));
     return res.status(401).end();
   }
   if (!JWT_SECRET) {
@@ -110,128 +106,52 @@ const execute = async function (req: Request, res: Response) {
           else if (argument.channel) channel = argument.channel;
           if (channel && cellularNumber) break;
         }
-        if (!channel || !cellularNumber ) return res.status(400).send('Input parameter is missing.');
+        if (!channel || !cellularNumber) return res.status(400).send('Input parameter is missing.');
 
         const now = new Date();
 
         let balanceValidationFailed = false;
         const offersRequestDurationTimestamps: DurationTimestampsPair = { start: performance.now(), end: null };
 
+        const {
+          API_URL,
+          API_SESSION_ID,
+          API_COUNTRY
+        } = process.env;
 
-        const httpsAgent = new https.Agent({ rejectUnauthorized: false });
-
-        // if (value === null || expiresAt < now) {
-        //   const { TOKEN_API_URL, TOKEN_API_USERNAME, TOKEN_API_PASSWORD } = process.env;
-  
-        //   console.log('GETTING TOKEN...');
-        //   const token: string = await axios({
-        //     method: 'post',
-        //     url: TOKEN_API_URL,
-        //     data: {
-        //       username: TOKEN_API_USERNAME,
-        //       password: TOKEN_API_PASSWORD
-        //     },
-        //     httpsAgent,
-        //   })
-        //     .then((res: any) => {
-        //       console.log('Token obtained.');
-        //       if (res.headers.authorization) return res.headers.authorization.substring(7);
-        //     })
-        //     .catch((err: any) => {
-        //       console.log('Error:');
-        //       console.log(err);
-        //     });
-        //   if (!token) balanceValidationFailed = true;
-        //   else {
-        //     req.app.locals.token = {
-        //       value: token,
-        //       expiresAt: new Date(now.getTime() + 1000 * 60 * 60 * 23),
-        //     };
-        //   }
-        // }
-
-        // let accountBalance = 0.0;
-  
-        // if (!balanceValidationFailed) {
-          const {
-            API_URL,
-            API_SESSION_ID,
-            API_COUNTRY
-          } = process.env;
-  
-        //   let phone: string | null = null;
-        //   for (const argument of decoded.inArguments) {
-        //     if (argument.phone) {
-        //       phone = argument.phone;
-        //       break;
-        //     }
-        //   }
-        //   if (!phone)  return res.status(400).send('Input parameter is missing.');
-  
-          // console.log('Getting balance data...');
-          
-          const offersApiResponse: { data: ResponseBody } | null = await axios({
-            method: 'post',
-            url: API_URL,
-            data: {
-                cellularNumber: Number(cellularNumber),
-                channel: channel
-            } as RequestBody,
-            headers: {
-                Country: API_COUNTRY,
-                'Session-Id': API_SESSION_ID
-            },
-            httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+        const offersApiResponse: { data: ResponseBody } | null = await axios({
+          method: 'post',
+          url: API_URL!,
+          data: {
+              cellularNumber: Number(cellularNumber),
+              channel: channel
+          } as RequestBody,
+          headers: {
+              Country: API_COUNTRY!,
+              'Session-Id': API_SESSION_ID!
+          },
+          httpsAgent: new https.Agent({ rejectUnauthorized: false }),
         })
-            .catch((err) => {
-                offersRequestDurationTimestamps.end = performance.now();
-                if (err.response) {
-                    const { data, status } = err.response;
-                    // specialConsoleLog({
-                    //     phoneNumber: cellularNumber!,
-                    //     eventName: 'OFFERS_REQUEST_FAILED',
-                    //     durationTimestamps: offersRequestDurationTimestamps,
-                    //     data: { data, status },
-                    // });
-                }
-                console.log('Error when calling the offers API:');
-                console.log(err);
-                return null;
-            });
+          .catch((err) => {
+              offersRequestDurationTimestamps.end = performance.now();
+              if (err.response) {
+                  const { data, status } = err.response;
+              }
+              console.log('Error when calling the offers API:');
+              console.log(err);
+              return null;
+          });
+
         offersRequestDurationTimestamps.end = performance.now();
 
-          // const saldoBalancesApiResponse = await axios({
-          //   method: 'post',
-          //   url: API_URL,
-          //   headers: {
-          //     Authorization: `Bearer ${req.app.locals.token.value}`,
-          //     Country: API_COUNTRY,
-          //   'Session-Id': API_SESSION_ID
-          //   },
-          //   httpsAgent,
-          // })
-          //   .then((res: any) => {
-          //     console.log('Response');
-          //     console.log(res.data);
-          //     return res.data;
-          //   })
-          //   .catch((err: any) => {
-          //     console.log('Error:');
-          //     console.log(err);
-          //   });
-          // if (!saldoBalancesApiResponse) balanceValidationFailed = true;
-          // else accountBalance = saldoBalancesApiResponse.balancesDetails.accountBalance;
+        // Enviar la respuesta de la API como respuesta al cliente
+        if (offersApiResponse) {
+          return res.status(200).json(offersApiResponse.data);
+        } else {
+          // Manejar el caso donde no hay respuesta de la API
+          return res.status(500).send('Error obteniendo respuesta de la API');
         }
-        
-  
-      //   res.status(200).send({
-      //     accountBalance,
-      //     balanceValidationFailed,
-      //   });
-      // } else {
-      //   console.error('inArguments invalid.');
-      //   return res.status(400).end();
-      // }
+      }
     },
   );
 };
