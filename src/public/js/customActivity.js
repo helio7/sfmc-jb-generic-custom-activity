d613359476b959d406302855f048837b9a6928d2
mateori0s <mateorios@live.com>
2024-03-26 12:13:40 -0300

    comentarios front y log 2

--- src/public/js/customActivity.js
+++ src/public/js/customActivity.js
@@ -14,24 +14,23 @@ define(['postmonger'], (Postmonger) => {
     connection.on('initActivity', (data) => {
         if (data) payload = data;
 
-        // const inArguments = Boolean(
-        //     data.arguments &&
-        //     data.arguments.execute &&
-        //     data.arguments.execute.inArguments &&
-        //     data.arguments.execute.inArguments.length > 0
-        // ) ? data.arguments.execute.inArguments : [];
+        const inArguments = Boolean(
+            data.arguments &&
+            data.arguments.execute &&
+            data.arguments.execute.inArguments &&
+            data.arguments.execute.inArguments.length > 0
+        ) ? data.arguments.execute.inArguments : [];
 
-        // const dataExtensionArg = inArguments.find(arg => arg.dataExtension);
-        // if (dataExtensionArg) document.getElementById('dataExtension').value = dataExtensionArg.dataExtension;
+        const dataExtensionArg = inArguments.find(arg => arg.dataExtension);
+        if (dataExtensionArg) document.getElementById('dataExtension').value = dataExtensionArg.dataExtension;
 
-        // const channelArg = inArguments.find(arg => arg.channel);
-        // if (channelArg) document.getElementById('channel').value = channelArg.channel;
+        const channelArg = inArguments.find(arg => arg.channel);
+        if (channelArg) document.getElementById('channel').value = channelArg.channel;
     });
 
     connection.on('clickedNext', () => {
         payload['arguments'].execute.inArguments = [
 
-            
             {   dataExtension : document.getElementById('dataExtension').value ,
                 channel : document.getElementById('channel').value,
                 cellularNumber : `{{Contact.Attribute."${dataExtension}".cellular_number}}` },
