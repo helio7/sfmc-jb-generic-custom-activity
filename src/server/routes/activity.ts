f7a9ffc29b8f906823109232a52a8147d084a0d8
mateori0s <mateorios@live.com>
2024-03-27 15:00:58 -0300

    modficacion de estructura activity.js

--- src/server/routes/activity.ts
+++ src/server/routes/activity.ts
@@ -1,6 +1,9 @@
+'use strict';
 import https from 'https';
 import axios from 'axios';
-import { Request, Response } from 'express';
+import { Request } from 'express';
+import { Response } from 'express';
+import { verify } from 'jsonwebtoken';
 
 const logExecuteData: {
   body: any;
@@ -24,7 +27,7 @@ const logExecuteData: {
 
 const saveData = (req: any) => {
   // Put data from the request in an array accessible to the main app.
-  logExecuteData.push({
+  exports.logExecuteData.push({
     body: req.body,
     headers: req.headers,
     trailers: req.trailers,
@@ -36,7 +39,7 @@ const saveData = (req: any) => {
     cookies: req.cookies,
     ip: req.ip,
     path: req.path,
-    host: req.hostname,
+    host: req.host,
     fresh: req.fresh,
     stale: req.stale,
     protocol: req.protocol,
@@ -45,108 +48,132 @@ const saveData = (req: any) => {
   });
 }
 
-interface RequestBody {
-  cellularNumber: number;
-  channel: string;
-  dataExtension: string;
-}
+interface InputParamenter {
+  dataExtension?: string;
+  channel?: string;
+  cellularNumber?: string;
 
-interface ResponseBody {
-  responseCode: number;
-  responseMessage: string;
-  handle: number;
-  pack: {
-    packId: string;
-    description: string;
-  }[];
+}
+interface DecodedBody {
+  inArguments?: InputParamenter[];
 }
 
 const execute = async function (req: Request, res: Response) {
-  try {
-    const { body } = req;
-    console.log('Request Body:', body);
+  const { body } = req;
 
-    const cellularNumber = 1121806490;
-    const channel = "PDC";
-    const dataExtension = "TestCA";
+  verify(
+    body.toString('utf8'),
+        async (err: any, decoded?: any) => {
+      if (err) {
+        console.error(err);
+        return res.status(401).end();
+      }
+      if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+        const { value, expiresAt } = req.app.locals.token;
 
-    // const dataExtension = body.dataExtension;
-    // const channel = body.channel;
-    // const cellularNumber = body.cellularNumber; 
+        const now = new Date();
 
+        let balanceValidationFailed = false;
 
-    console.log('Cellular Number:', cellularNumber);
-    console.log('Data Extension:', dataExtension);
-    console.log('Channel:', channel);
+        const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+        const {
+          API_URL,
+          API_SESSION_ID,
+          API_COUNTRY
+        } = process.env;
+      
 
-    if (!dataExtension || !channel || !cellularNumber) {
-      console.error(new Error('Missing input parameters'));
-      return res.status(400).send('Missing input parameters');
+          if (!balanceValidationFailed) {
+
+          let cellularNumber: string | null = null;
+          for (const argument of decoded.inArguments) {
+            if (argument.cellularNumber) {
+              cellularNumber = argument.cellularNumber;
+              break;
+            }
+            let channel: string | null = null;
+            for (const argument of decoded.inArguments) {
+              if (argument.channel) {
+                channel = argument.channel;
+                break;
+              }
+          }
+          let dataExtension: string | null = null;
+          for (const argument of decoded.inArguments) {
+            if (argument.dataExtension) {
+              channel = argument.channel;
+              break;
+            }
+        }
+          
+
+          console.log('Cellular Number:', cellularNumber);
+          console.log('Data Extension:', dataExtension);
+          console.log('Channel:', channel);
+
+          if (!cellularNumber || !channel) return res.status(400).send('Input parameter is missing.');
+  
+          console.log('Llamando a la API...');
+          const packRenovableApiResponse: null = await axios({
+            method: 'post',
+            url: API_URL!,
+            data: {
+              cellularNumber: cellularNumber,
+              channel: channel
+            },
+            headers: {
+              Country: API_COUNTRY!,
+              'Session-Id': API_SESSION_ID!
+            },
+            httpsAgent,
+          })
+            .then((res: any) => {
+              console.log('Response');
+              console.log(res.data);
+              return res.data;
+            })
+            .catch((err: any) => {
+              console.log('Error:');
+              console.log(err);
+            });
+          if (!packRenovableApiResponse) balanceValidationFailed = true;
+        }
+  
+        res.status(200).send({
+          balanceValidationFailed,
+        });
+      } else {
+        console.error('inArguments invalid.');
+        return res.status(400).end();
+      }
     }
-
-    const now = Date.now();
-    const offersRequestDurationTimestamps = { start: now, end: null as null | number };
-
-    const {
-      API_URL,
-      API_SESSION_ID,
-      API_COUNTRY
-    } = process.env;
-
-    console.log('Llamando a la API...');
-    const packRenovableApiResponse: { data: ResponseBody } | null = await axios({
-      method: 'post',
-      url: API_URL!,
-      data: {
-        cellularNumber: cellularNumber,
-        channel: channel
-      } as RequestBody,
-      headers: {
-        Country: API_COUNTRY!,
-        'Session-Id': API_SESSION_ID!
-      },
-      httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-    });
-
-    offersRequestDurationTimestamps.end = Date.now();
-
-    if (packRenovableApiResponse) {
-      console.log('Respuesta de API:', packRenovableApiResponse.data);
-      return res.status(200).json(packRenovableApiResponse.data);
-    } else {
-
-      console.error('Sin respuesta de la API');
-      return res.status(500).send('Error obteniendo respuesta de la API');
-    }
-  } catch (error) {
-    console.error('Error en la ejecución:', error);
-    return res.status(500).send('Error en la ejecución');
-  }
+    },
+  );
 };
 
 const edit = (req: any, res: any) => {
   saveData(req);
-  res.status(200).send('Edit');
+  res.send(200, 'Edit');
 };
 
 const save = (req: any, res: any) => {
   saveData(req);
-  res.status(200).send('Save'); 
+  res.send(200, 'Save');
 };
 
 const publish = (req: any, res: any) => {
   saveData(req);
-  res.status(200).send('Publish');
+  res.send(200, 'Publish');
 };
 
 const validate = (req: any, res: any) => {
   saveData(req);
-  res.status(200).send('Validate');
+  res.send(200, 'Validate');
 };
 
 const stop = (req: any, res: any) => {
   saveData(req);
-  res.status(200).send('Stop');
+  res.send(200, 'Stop');
 };
 
 export default {
